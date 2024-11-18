ede4be34656bc32f4fcef19212e9933820e85f35
EnriqueLeonR <151400078+EnriqueLeonR@users.noreply.github.com>
2024-11-18 17:00:59 -0500

    Update CardGameApplication.java

--- src/main/java/com/example/groupprojectcardgame/CardGameApplication.java
+++ src/main/java/com/example/groupprojectcardgame/CardGameApplication.java
@@ -10,9 +10,7 @@ public class CardGameApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        // Use a debug statement to confirm the FXML file's path
-        System.out.println(getClass().getResource("/com/example/groupprojectcardgame/GameScreen.fxml"));
-
+        
         // Load the FXML file
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/groupprojectcardgame/GameScreen.fxml"));
         Parent root = loader.load();
